5325ab32310f18fb11b194b090677210a919971f
adhhamdev <msmadhhamg@gmail.com>
2024-04-22 21:20:16 +0530

    Add sharp dependency to package.json and update UI content in root-ui.jsx

--- components/component/root-ui.jsx
+++ components/component/root-ui.jsx
@@ -200,7 +200,7 @@ export function RootUI() {
             </a>
             <a
               className="inline-flex h-9 items-center justify-center rounded-md border border-slate-200  bg-white shadow-sm px-4 text-sm font-medium transition-colors hover:bg-gray-100 hover:text-gray-900 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-gray-950 disabled:pointer-events-none disabled:opacity-50 dark:bg-gray-950 dark:hover:bg-gray-800 dark:hover:text-gray-50 dark:focus-visible:ring-gray-300 dark:border-slate-800"
-              href="https://drive.google.com/file/d/1nJJXINjPP8KVjHibXZc8XoSSXjwFheDI/view?usp=sharing"
+              href="https://drive.google.com/file/d/1nJJXINjPP8KVjHibXZc8XoSSXjwFheDI"
               download
             >
               <FileText className="mr-2" />
@@ -214,11 +214,11 @@ export function RootUI() {
               About Me
             </h2>
             <p className="max-w-prose text-gray-500 md:text-xl/relaxed dark:text-gray-400 p-8">
-              Recent Computer Science Graduate with a passion for developing
-              scalable web applications and working across the full stack.
-              I&apos;m looking to join forces with any size organization to grow
-              my skill set while contributing to the positive outcome of
-              products that are offered by the Company.
+              Highly motivated BTEC HND in Software Engineering student and self-taught Full Stack Developer with a
+              strong focus on building websites with exceptional SEO and
+              accessibility. Skilled in creating visually attractive and
+              high-performance websites. Seeking opportunities to contribute my
+              expertise and creativity.
             </p>
           </div>
           <div className="flex flex-col gap-6 mb-12" id="skills">
@@ -305,19 +305,28 @@ export function RootUI() {
                   <Label className="text-base" htmlFor="name">
                     Name
                   </Label>
-                  <Input id="name" required />
+                  <Input id="name" required placeholder="Enter your name..." />
                 </div>
                 <div className="grid gap-2">
                   <Label className="text-base" htmlFor="email">
                     Email
                   </Label>
-                  <Input id="email" required type="email" />
+                  <Input
+                    id="email"
+                    required
+                    type="email"
+                    placeholder="Enter your email..."
+                  />
                 </div>
                 <div className="grid gap-2">
                   <Label className="text-base" htmlFor="message">
                     Message
                   </Label>
-                  <Textarea id="message" required />
+                  <Textarea
+                    id="message"
+                    required
+                    placeholder="Enter your message..."
+                  />
                 </div>
                 <Button type="submit">
                   Submit
@@ -350,7 +359,7 @@ export function RootUI() {
             GitHub
           </a>
           <a
-          target="_blank"
+            target="_blank"
             className="text-xs hover:underline underline-offset-4 flex flex-col items-center"
             href="https://www.linkedin.com/in/adhham/"
           >
