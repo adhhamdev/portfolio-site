1201138ac8cad1f47e0dab1a79c56d8d09a1f1f4
Adhham Safwan <msmadhhamg@gmail.com>
2024-07-20 20:37:23 +0000

    Fix non interactivity

--- app/page.js
+++ app/page.js
@@ -3,30 +3,26 @@ import { RootUI } from "@/components/component/root-ui";
 import Script from "next/script";
 
 export default function Home() {
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      resolve(
-        <div>
-          <RootUI />
-          <Script type="text/javascript" id="inline-form-script">
-            {function configureAhoy() {
-              ahoy.configure({
-                visitsUrl: "https://usebasin.com/ahoy/visits",
-                eventsUrl: "https://usebasin.com/ahoy/events",
-                page: "437c48626f77",
-              });
-              ahoy.trackView();
-              ahoy.trackSubmits();
-            }}
-          </Script>
-          <Script
-            src="https://cdn.jsdelivr.net/npm/ahoy.js@0.3.9/dist/ahoy.min.js"
-            async
-            defer
-            onLoad="configureAhoy()"
-          ></Script>
-        </div>
-      );
-    }, 2000);
-  });
+  return (
+    <div>
+      <RootUI />
+      <Script
+        src="https://cdn.jsdelivr.net/npm/ahoy.js@0.3.9/dist/ahoy.min.js"
+        defer
+        async
+        onLoad="configureAhoy"
+      ></Script>
+      <Script type="text/javascript" id="inline-form-script">
+        {function configureAhoy() {
+          ahoy.configure({
+            visitsUrl: "https://usebasin.com/ahoy/visits",
+            eventsUrl: "https://usebasin.com/ahoy/events",
+            page: "437c48626f77",
+          });
+          ahoy.trackView();
+          ahoy.trackSubmits();
+        }}
+      </Script>
+    </div>
+  );
 }
